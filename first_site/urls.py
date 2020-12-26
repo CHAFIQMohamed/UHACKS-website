cad778cd1856b5c1f79813fd26432904f704814d
CHAFIQMohamed <MohamedCHAFIQ@um6p.ma>
2020-12-27 00:13:03 +0100

    commit

--- first_site/urls.py
+++ first_site/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path,include
 from main import views as v
 
+from django.views.static import serve
+from django.conf.urls import url
+
+
 urlpatterns = [
     #path for admin
     path('admin/', admin.site.urls),
     #path for home
     path('', include("main.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
