bf5c017bfc5542e2f0d05af4e62d24057155921a
CHAFIQMohamed <Mohamed.CHAFIQ@um6p.ma>
2020-12-29 11:38:48 +0100

    fist site

--- first_site/settings.py
+++ first_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v%+z-5-qku8k!=#e_%k)j)@a*5x206sa_bu155uqgsoi2qi938'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','uhacks-project.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
